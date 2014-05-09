da44647d35aba073eaf6e09e98d7cf926d23f4c5
DanielDowd <041401143@my.central.wa.edu.au>
2014-05-09 17:45:54 +0800

    Update FuelPurchase.java

--- FuelPurchase.java
+++ FuelPurchase.java
@@ -1,16 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package carrental;
+
+/**
+ *
+ * @author Daniel
+ */
 public class FuelPurchase {
-	private double	fuelEconomy;
+	
+        private double fuelPrice;       //variable to hold price of fuel per litre - Daniel
+        private double fuelAmount;      //variable to hold litres of fuel purchased - Daniel
 
-	public FuelPurchase(double fuelEconomy) {
-		this.fuelEconomy = fuelEconomy;
+	public FuelPurchase(double fuelPrice, double fuelAmount) {
+		this.fuelPrice = fuelPrice;
+                this.fuelAmount = fuelAmount;
 	}
 
+        //Method to get total amount spent on fuel - Daniel
+        public double getTotalCost() {
+            return this.fuelPrice * fuelAmount;
+        }
+        
+        //Method to get fuel economy in litres per 100km - Daniel
 	public double getFuelEconomy() {
-		return fuelEconomy;
+		return this.fuelAmount / 100;
 	}
 
-	public void setFuelEconomy(double fuelEconomy) {
-		this.fuelEconomy = fuelEconomy;
-	}
+         //Getter and setter for fuelPrice variable - Daniel      
+        public double getFuelPrice() {
+            return fuelPrice;
+        }
+        
+        public void setFuelPrice() {
+            this.fuelPrice = fuelPrice;
+        }
+        
+        //Getter and setter for fuelAmount - Daniel
+        public double getFuelAmount() {
+            return fuelAmount;
+        }
+        
+        public void setFuelAmount() {
+            this.fuelAmount = fuelAmount;
+        }
+	
 }
